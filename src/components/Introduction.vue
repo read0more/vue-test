03b3d8b5206b765a5236cdb151667532bb9df38e
ykpark <read0more@gmail.com>
2022-02-01 21:55:18 +0900

    Introduction 내용들

---
+++ src/components/Introduction.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+const title = "this is title";
+const message = ref("reverse message");
+const input = ref("");
+const messages = ["메시지1", "메시지2", "메시지3", "메시지4"];
+
+const reverse = () => {
+  message.value = message.value.split("").reverse().join("");
+};
+setInterval(() => {
+  count.value++;
+}, 1000);
+</script>
+
+<template>
+  <span v-bind:title="title">Counter: {{ count }}</span>
+  <div v-on:click="reverse">{{ message }}</div>
+  <div>
+    <span>two way binding</span>
+    <p>{{ input }}</p>
+    <input v-model="input" />
+    <div v-if="input">if로 input값 있을때만 보이는</div>
+  </div>
+  <ul style="display: inline-block">
+    <li v-for="message in messages">
+      {{ message }}
+    </li>
+  </ul>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.global-scope {
+  color: cyan;
+}
+</style>
