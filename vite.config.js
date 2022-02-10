dc35e8569c47d4c22b4611f349c8c5bfa2fc226e
ykpark <read0more@gmail.com>
2022-02-10 12:06:30 +0900

    alias for IDE

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const path = require('path')
+const pathAlias = require('./path.alias.js');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve:{
     alias:{
-      '@' : path.resolve(__dirname, './src')
+      ...pathAlias
     },
   },
   plugins: [vue()]
